d8e3b9f12de66a85265248200c9cfdf9ef584971
rashu-pro <rashu.pro@gmail.com>
2024-10-05 00:05:17 +0600

    header style for account pages has been changed

--- wp-content/themes/blocksy-child/functions.php
+++ wp-content/themes/blocksy-child/functions.php
@@ -6,4 +6,6 @@ if (! defined('WP_DEBUG')) {
 
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
 });
