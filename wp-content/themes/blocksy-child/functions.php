c7a18abad16b30581748ceddac4f4e69994bed5c
rashu-pro <rashu.pro@gmail.com>
2024-10-23 23:54:01 +0600

    shortcode to redirect user to the dashboard after login written

--- wp-content/themes/blocksy-child/functions.php
+++ wp-content/themes/blocksy-child/functions.php
@@ -7,7 +7,7 @@ if (! defined('WP_DEBUG')) {
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css');
 
-  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ), '1.0.2');
+  wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ), '1.0.5');
 });
 
 function custom_login_redirect( $redirect_to, $request, $user ) {
@@ -23,4 +23,14 @@ function custom_login_redirect( $redirect_to, $request, $user ) {
   return $redirect_to;
 }
 add_filter( 'login_redirect', 'custom_login_redirect', 10, 3 );
+function check_login_and_redirect() {
+    // Check if the user is logged in
+    if (is_user_logged_in()) {
+        // Redirect to the specific page
+        wp_redirect(home_url('/dashboard/'));
+        exit;
+    }
+}
+add_shortcode('custom_login_redirect', 'check_login_and_redirect');
+
 
